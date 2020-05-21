43a1c3d09740d386ddcf2a6c5ed5b3b0eadf4517
guigranja <admsdballetgo@gmail.com>
2020-05-20 23:56:04 -0300

    Terminando parte do curso Interagindo com o DOM

---
+++ Interagindo-com-DOM/desafio-04/app.js
@@ -0,0 +1,39 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        classe1: 'destaque',
+        perigo: true,
+        classe3: '',
+        classe4: '',
+        cor5: '',
+        estilo5: {
+            width: '100px',
+            height: '100px'
+        },
+        width: '0',
+        tempo: ''
+    },
+    methods: {
+        iniciarEfeito() {
+            setInterval(() => {
+                this.classe1 = this.classe1 == 'destaque' ? 'encolher' : 'destaque'
+            }, 1000)
+        },
+        iniciarProgresso() {
+            let valor = 0;
+            const temporizador = setInterval(() => {
+                valor += 10
+                this.width = `${valor}%`
+                if(valor == 100) clearInterval(temporizador)
+                this.tempo = `${valor}%`;
+            }, 500);
+        },
+        setPerigo(e) {
+            if(event.target.value == 'true') {
+                this.perigo = true
+            } else if (event.target.value == "false") {
+                this.perigo = false
+            }
+        }
+    }
+})
