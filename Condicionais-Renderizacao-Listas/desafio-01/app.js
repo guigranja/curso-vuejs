f8b690789a8fdbed946b56f5e0dfb2cd5f80f848
guigranja <admsdballetgo@gmail.com>
2020-05-21 21:37:30 -0300

    Seção 3 - Usando Condicionais e Renderização de Listas

---
+++ Condicionais-Renderizacao-Listas/desafio-01/app.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        expressao: true,
+        nomes: ['Guilherme', 'Rafa', 'Ana'],
+        livros: {
+            titulo: 'O Senhor dos Aneis',
+            autor: 'J.R.R. Tolkiens',
+            volume: '3'
+        },
+        alunos: {
+            id: 10,
+            nome: 'Guilherme',
+            notas: [7.67, 8.33, 9.55, 10]
+        }
+    }
+})
