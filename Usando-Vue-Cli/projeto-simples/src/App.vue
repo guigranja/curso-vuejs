a573ff95058cccbc40de52ce59bd98d9c3d93c65
guigranja <admsdballetgo@gmail.com>
2020-05-29 09:53:55 -0300

    Fluxo de Desenvolvimento Real Usando Vue Cli

---
+++ Usando-Vue-Cli/projeto-simples/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+    <br>
+    <button @click="titulo += ' #'">Alterar</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    // Pode ter watch, computed, data, methods
+
+    data: function () {
+      return {
+        titulo: 'Teste Data usando Vue JS'
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    background-color: #42b983;
+    color: #ffffff;
+    font-weight: bolder;
+  }
+</style>
